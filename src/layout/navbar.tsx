ed39aa82b8829e45bc5210195ec53a5895be9c1a
webworkcoder <webwork106@gmail.com>
2026-04-20 13:10:19 +0530

    Update navbar.tsx

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -181,7 +181,7 @@ export const Navbar = () => {
           className="flex-shrink-0 transition-transform active:scale-95"
         >
           <img
-            src="/Elogo.png"
+            src="/eLogo.png"
             alt="Topson Education"
             className="h-10 md:h-14 w-auto object-contain"
           />
