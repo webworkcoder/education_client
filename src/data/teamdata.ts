4dc6e158cbed1cd93a79bff14adf027dc684f669
AnkitGupta <webwork106@gmail.com>
2026-06-24 11:26:21 +0530

    fix image

--- src/data/teamdata.ts
+++ src/data/teamdata.ts
@@ -1,5 +1,5 @@
 export const team = [
-  { image: "/founder/ceo.jpeg", name: "CEO and founder of company", role: "CEO and founder of company" },
+  { image: "/founder/ceo.jpeg", name: "Dr Saif", role: "CEO and founder of company" },
   { image: "/founder/team_head.jpeg", name: "Dr madhurendar Kumar", role: "team head Bihar" },
   { image: "/founder/uttrakhand_head.jpeg", name: "Dr Anshu tiwari", role: "team head uttrakhand" },
   { image: "/founder/director.jpeg ", name: "Dr Masroor", role: "Director management" },
